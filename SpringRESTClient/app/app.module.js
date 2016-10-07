c49962cdc5b0ea2d489104b935cbff240a59bcd9
George Berar <berar1994@gmail.com>
2016-10-07 10:36:32 +0300

    modified login-view background color and added route

--- SpringRESTClient/app/app.module.js
+++ SpringRESTClient/app/app.module.js
@@ -2,4 +2,14 @@
 
 	var app = angular.module('sboxApp', ['ui.bootstrap','sboxAppRoutes','welcomeViewModule','loginViewModule']);
 
+	/**
+     * go to location when using buttons   
+     */
+    app.controller('RedirectToController', ['$location', function($location){
+        this.redirectTo = function(routePath){
+            $location.path(routePath);
+        };
+    } ]);
+
+
 })();
--- SpringRESTClient/app/components/welcome/welcomeView.module.js
+++ SpringRESTClient/app/components/welcome/welcomeView.module.js
@@ -18,14 +18,4 @@
     });
 
 
-    /**
-     * go to location when using buttons   
-     */
-    app.controller('RedirectToController', ['$location', function($location){
-        this.redirectTo = function(routePath){
-            $location.path(routePath);
-        };
-    } ]);
-
-
 })();
