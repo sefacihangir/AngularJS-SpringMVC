7a5a4a20ed4d6da1e76eaa796ad26bbdd4d3c8cc
George Berar <berar1994@gmail.com>
2016-10-26 13:58:56 +0300

    Address binding not working when trying to add address

--- SpringRESTClient/app/app.module.js
+++ SpringRESTClient/app/app.module.js
@@ -2,6 +2,18 @@
 
 	var app = angular.module('sboxApp', ['ui.bootstrap','sboxAppRoutes','welcomeViewModule','loginViewModule', 'signupViewModule']);
 
+    /**
+     * global constants
+     */
+    app.constant('config', {
+        PHONE_PREFIX: '+40',
+        PRIMARY_ADDRESS: 'primary',
+        OTHER_ADDRESS: 'other',
+        LANGUAGE: 'EN'
+    });
+
+
+
 	/**
      * go to location when using buttons   
      */
@@ -10,6 +22,100 @@
             $location.path(routePath);
         };
     } ]);
+
+
+
+    /**
+     * directive to verify if the passwords match
+     */
+    app.directive('passwordMatch', function(){
+    	return {
+    		restrict: 'A',
+    		scope: true,
+            require: 'ngModel',
+            link: function(scope, elem, attrs, control){
+                var firstPassword = '#' + attrs.passwordMatch;
+                $(elem).add(firstPassword).on('keyup', function(){
+                    scope.$apply(function(){
+                        var v = elem.val() === $(firstPassword).val();
+                        control.$setValidity('match', v);
+                    });
+                });
+            }
+    	};
+    });
+
+
+
+    /**
+     * directive to validate the phone number
+     */
+    app.directive('phoneValidation', function(){
+        return{
+            restrict: 'A',
+            scope: true,
+            require: 'ngModel',
+            link: function(scope, elem, attrs, control){
+                var checker = function(){
+                    var RE = /^[\d\.\-]+$/;
+                    var phone;
+                    if((phone = scope.$eval(attrs.ngModel)) != undefined){
+                        // check the length
+                        if(phone.length >= 0 && phone.length < 9)
+                            return false;
+                        if(!RE.test(phone))
+                            return false;
+
+                        return true;
+                    } else{
+                        return false;
+                    }
+                };
+
+                scope.$watch(checker, function(n){
+                    control.$setValidity("valid", n);
+                });
+            }
+        };
+    });
+
+
+
+    /**
+     * used to open any modal and assign a controller to it
+     * controllerAs used as an alias for ModalInstanceController; in order to use the methods from it type
+     * modalInstanceCtrl.ok() || modalInstanceCtrl.cancel()
+     */
+    app.controller('ModalController', function($uibModal){
+
+		 this.openModal = function(urlForModalTemplate, size){
+		 	var uibModalInstance = $uibModal.open({
+		 		templateUrl: urlForModalTemplate,
+		 		controller: 'ModalInstanceController',
+		 		controllerAs: 'modalInstanceCtrl',
+		 		size: size
+		 	});
+		 };
+
+	});
+
+
+    /**
+     * used to handle the dismiss of the opened modal
+     */
+	app.controller('ModalInstanceController', function($uibModalInstance){
+		
+		this.ok = function(){
+			$uibModalInstance.close();
+		};
+
+		this.cancel = function(){
+			$uibModalInstance.dismiss();
+		};
+
+	});
+
+
 
 
 })();
