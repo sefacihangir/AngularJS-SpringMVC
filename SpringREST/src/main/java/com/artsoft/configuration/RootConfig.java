4afb3a0ec8d5daf71cfcdd5bf9505f7e639b1c62
George Berar <berar1994@gmail.com>
2016-11-08 15:52:32 +0200

    info: deleted old configuration beans that were commented

--- SpringREST/src/main/java/com/artsoft/configuration/RootConfig.java
+++ SpringREST/src/main/java/com/artsoft/configuration/RootConfig.java
@@ -2,23 +2,15 @@ package com.artsoft.configuration;
 
 import java.util.Properties;
 
-import javax.persistence.SharedCacheMode;
-import javax.persistence.ValidationMode;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -83,52 +75,6 @@ public class RootConfig {
         return resolver;
     }
 	
-	
-//	@Bean
-//	public HibernateJpaVendorAdapter hibernateJpaVendorAdapter() {
-//		return new HibernateJpaVendorAdapter();
-//	}
-//
-//	@Bean
-//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-//
-//		entityManagerFactory.setDataSource(dataSource());
-//		entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter());
-//		entityManagerFactory.setJpaProperties(jpaProperties());
-//		entityManagerFactory.setPackagesToScan("com.artsoft.model");
-//		entityManagerFactory.setSharedCacheMode(SharedCacheMode.ENABLE_SELECTIVE);
-//		entityManagerFactory.setValidationMode(ValidationMode.NONE);
-//
-//		return entityManagerFactory;
-//	}
-//
-//
-//
-//	@Bean
-//	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-//		return new PersistenceExceptionTranslationPostProcessor();
-//	}
-//
-//	@Bean
-//	public PlatformTransactionManager transactionManager() {
-//		JpaTransactionManager transactionManager = new JpaTransactionManager();
-//		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-//		return transactionManager;
-//	}
-//
-//
-//
-//	private Properties jpaProperties() {
-//		Properties properties = new Properties();
-//
-//		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-//		properties.put("hibernate.show_sql", "false");
-//		properties.put("hibernate.id.new_generator_mappings", "false");
-//		properties.put("hibernate.enable_lazy_load_no_trans", "true");
-//
-//		return properties;
-//	}	
 	
 	
 }
