d366e9b08e7fc2fa48e9f495d1a3e7495fc4ce37
George Berar <berar1994@gmail.com>
2016-11-18 15:33:15 +0200

    test: tested getUploadFolderPath() method from PropertiesUtil.class

--- SpringREST/src/main/java/com/artsoft/util/TestDate.java
+++ SpringREST/src/main/java/com/artsoft/util/TestDate.java
@@ -1,10 +1,45 @@
 package com.artsoft.util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TestDate {
 
 	public static void main(String[] args) {
-		System.out.println(DateUtil.getCurrentTimestamp());
+		TestDate test = new TestDate();
+		try {
+			System.out.println("PATH -- " + test.getUploadFolderPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
+	public String getUploadFolderPath() throws IOException {
+
+		String path = "";
+
+		try {
+			Properties properties = new Properties();
+			String propertiesFileName = "application.properties";
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propertiesFileName);
+
+			if (inputStream != null) {
+				properties.load(inputStream);
+			} else {
+				throw new FileNotFoundException(
+						"Property file '" + propertiesFileName + "' not found in the classpath");
+			}
+
+			path = properties.getProperty("upload_folder_path");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return path;
 	}
 
 }
