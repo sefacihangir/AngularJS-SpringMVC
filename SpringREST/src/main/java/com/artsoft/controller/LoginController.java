41aa49b49dde5a0fe11a732bed44173a88960b0d
George Berar <berar1994@gmail.com>
2016-11-18 14:31:53 +0200

    reinforcement: login with spring security context

--- SpringREST/src/main/java/com/artsoft/controller/LoginController.java
+++ SpringREST/src/main/java/com/artsoft/controller/LoginController.java
@@ -6,6 +6,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +22,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.artsoft.error.CustomError;
 import com.artsoft.model.AppUser;
+import com.artsoft.model.Role;
 import com.artsoft.service.AppUserService;
-import com.artsoft.util.PasswordUtil;
 
 @RestController
 @RequestMapping("/login_control")
@@ -25,31 +33,56 @@ public class LoginController {
 	@Autowired
 	AppUserService appUserService;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserDetailsService customUserDetailsService;
+
 	@RequestMapping(value = "/login", headers = {"Accept=*/*" }, produces = "application/json", method = RequestMethod.POST)
-	public Object login(@RequestParam(value = "email") String email,
-						@RequestParam(value = "password") String password) {
+	public Object login(@RequestParam(value = "email") String email, @RequestParam(value = "password") String password) {
 
-		AppUser user = appUserService.findByEmail(email);
+		// clear the security context
+		SecurityContextHolder.clearContext();
+		
 		Map<String, Object> response = new HashMap<String, Object>();
+		UserDetails userDetails = null;
+		
+		try{
+			userDetails = customUserDetailsService.loadUserByUsername(email);
+		}catch(UsernameNotFoundException ex){
+			 CustomError error = new CustomError();
+			 error.setHasError(true);
+			 error.setErrorOnField("email");
+			 error.setErrorMessage("Incorrect email.");
+			 response.put("error", error);
+			 return new ResponseEntity<Map<String, Object>>(response, HttpStatus.FORBIDDEN);
+		}
 
-		if (user == null) {
-			CustomError error = new CustomError();
-			error.setHasError(true);
-			error.setErrorOnField("email");
-			error.setErrorMessage("Invalid email " + email + ".");
-			response.put("error", error);
-		} else {
-			String encryptedInputPassword = PasswordUtil.encryptPassword(password);
-			if (user.getPassword().equals(encryptedInputPassword)) {
-				response.put("user", user);
-			} else {
-				CustomError error = new CustomError();
-				error.setHasError(true);
-				error.setErrorOnField("password");
-				error.setErrorMessage("Invalid password.");
-				response.put("error", error);
+		
+		if ( (userDetails != null) && passwordEncoder.matches(password, userDetails.getPassword()) ) {
+			Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(auth);
+			
+			AppUser user = new AppUser();
+			user.setEmail(userDetails.getUsername());
+			
+			for(GrantedAuthority r : userDetails.getAuthorities()){
+				Role role = new Role();
+				role.setRoleName(r.getAuthority());
+				user.setRole(role);
 			}
-
+			
+			
+			response.put("user", user);
+		}
+		else{
+			 CustomError error = new CustomError();
+			 error.setHasError(true);
+			 error.setErrorOnField("password");
+			 error.setErrorMessage("Incorrect password.");
+			 response.put("error", error);
+			 return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
 		}
 
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
